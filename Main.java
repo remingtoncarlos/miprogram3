4af4b67d138d7acf1518d96c039cda72c1fc34c9
Carlos Alegria <carlos.alegria.7155@miremington.edu.co>
2022-08-08 17:20:08 -0500

    Modificaciones del archivo main.java 05-08-2022

--- Main.java
+++ Main.java
@@ -1,32 +1,32 @@
-/*             FUNDMENTOS DE PROGRAMACI”N AVANZADA 1
-                           TAREA UNIDAD 1
+/*             PARA INGENIERRIA DL SOFTWARE 2
+                           TAREA UNIDAD 2
                            Por:  CARLOS ALBERTO ALEGRIA V
-                           Docente: iNG. NIXON DUARTE ACOSTA
-                           Fecha: 19-02-2022
-                           Popay·n
+                           Docente: iNG. YURY MONTOYA
+                           Fecha: 08/082022
+                           Popay√Ån
  */
 
-package alquilervehiculos2;
+package miprogram3;
+
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-  //  char salir='s';
-    int x=0;
-	int vr=0;
-    
- //   Scanner entrar=new Scanner(System.in);
-		
-		Metodos m=new Metodos();
-		System.out.print("BIENVENIDO A ALQUIATUOS S.A.S\n");
-		
-		do{   
-			vr=m.menu(x);
-//			System.out.println("Vr: "+vr+"\n");
-	
-		
-		}while(vr!=4); //salir=='s');
-	}//fin main
+		// char salir='s';
+		int x = 0;
+		int vr = 0;
 
-}//fin clase
+		// Scanner entrar=new Scanner(System.in);
+
+		Metodos m = new Metodos();
+		System.out.print("BIENVENIDO A ALQUIATUOS S.A.S\n");
+
+		do {
+			vr = m.menu(x);
+			// System.out.println("Vr: "+vr+"\n");
+
+		} while (vr != 4); // salir=='s');
+	}// fin main
+
+}// fin clase
